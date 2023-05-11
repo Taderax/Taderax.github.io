d3a1764b97b004703893f10a767c63fe8abfbb57
got ur token <127048552+Taderax@users.noreply.github.com>
2023-05-11 18:19:16 +0100

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ class _app {
     backgroundToggler = false;
     shouldIgnoreVideo = false;
     effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-    brandDescription = ['capitalist', 'skid', 'white boy', 'dahoodian', 'sound geezer', '.gg/mp4'];
+    brandDescription = ['capitalist', 'skid', 'white boy', 'dahoodian', 'sound geezer', '.gg/lostsoulsw'];
   
     titleChanger = (text, delay) => {
       if (!text) return;
